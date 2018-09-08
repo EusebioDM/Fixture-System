1807815253bddbf11e88ad5f9c61ecd2447a7686
EusebioDM <eusebioDM98@gmail.com>
2018-09-08 19:28:56 -0300

    Created CreateTeamTest in TeamTest

---
+++ EirinDuran/EirinDuran.Test/TeamTest.cs
@@ -0,0 +1,43 @@
+﻿using EirinDuran.Test.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+using Image = System.Drawing.Image;
+
+namespace EirinDuran.Test
+{
+    [TestClass]
+    public class TeamTest
+    {
+
+        [TestMethod]
+        public void CreateTeamNameTest()
+        {
+            Team boca = CreateBocaTeam();
+            Assert.AreEqual("Boca Juniors", boca.Name);
+        }
+
+        [TestMethod]
+        public void CreateTeamImageTest()
+        {
+            Team boca = CreateBocaTeam();
+            Assert.AreEqual(GetImage(Resources.Boca, boca.Image);
+        }
+
+        private Team CreateBocaTeam()
+        {
+            string name = "Boca Juniors";
+            Image image = GetImage(Resources.Boca);
+            Team boca = new Team(name, image);
+        }
+
+        private Image GetImage(byte[] resource)
+        {
+            return new Bitmap(new MemoryStream(resource));
+        }
+    }
+}
