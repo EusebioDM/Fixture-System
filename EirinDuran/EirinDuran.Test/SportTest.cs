f0a9400fb9175b196f3032f41603e731b4af14cd
EusebioDM <eusebioDM98@gmail.com>
2018-09-09 15:35:33 -0300

    Created AddTeamTest in SportTEst

--- EirinDuran/EirinDuran.Test/SportTest.cs
+++ EirinDuran/EirinDuran.Test/SportTest.cs
@@ -1,7 +1,9 @@
-﻿using EirinDuran.Domain.Fixture;
+﻿using EirinDuran.Domain;
+using EirinDuran.Domain.Fixture;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EirinDuran.Test
@@ -22,6 +24,16 @@ namespace EirinDuran.Test
         public void CreateTeamEmptyNameTest()
         {
             Sport sport = new Sport("    ");
+        }
+
+        [TestMethod]
+        public void AddTeamTest()
+        {
+            Sport sport = new Sport("Futbol");
+            Team boca = new Team("Boca", null);
+            Team river = new Team("River", null);
+            IEnumerable<Team> actual = sport.Teams;
+            Assert.IsTrue(actual.Contains(boca) && actual.Contains(river));
         }
     }
 }
