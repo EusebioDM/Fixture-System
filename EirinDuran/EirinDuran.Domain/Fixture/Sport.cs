491defe229d467c5be05bf9ebcf9f5dc83884e13
EusebioDM <eusebioDM98@gmail.com>
2018-09-09 14:27:43 -0300

    Created Empty Sport clases

---
+++ EirinDuran/EirinDuran.Domain/Fixture/Sport.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EirinDuran.Domain.Fixture
+{
+    internal class Sport
+    {
+    }
+}
--- EirinDuran/EirinDuran.Test/TeamTest.cs
+++ EirinDuran/EirinDuran.Test/TeamTest.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Text;
-using Image = System.Drawing.Image;
 
 namespace EirinDuran.Test
 {
