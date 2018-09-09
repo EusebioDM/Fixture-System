968b49efc27a9638cf105d1649f7884142c30e40
EusebioDM <eusebioDM98@gmail.com>
2018-09-09 15:30:12 -0300

    Created Sport Class

--- EirinDuran/EirinDuran.Domain/Fixture/Sport.cs
+++ EirinDuran/EirinDuran.Domain/Fixture/Sport.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace EirinDuran.Domain.Fixture
 {
-    internal class Sport
+    public class Sport
     {
+        private string name;
+        private StringValidator validator;
+        public string Name { get => name; set => SetNameIfValid(value); }
+
+        public Sport(string name)
+        {
+            validator = new StringValidator();
+            Name = name;
+        }
+
+        private void SetNameIfValid(string value)
+        {
+            bool valid = validator.ValidateNotNullOrEmptyString(value);
+            if (!valid)
+            {
+                throw new EmptyFieldException();
+            }
+            else
+            {
+                name = value;
+            }
+        }
     }
 }
