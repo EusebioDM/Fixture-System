de9d502899c8ccb51db03e80856c3eae98125795
EusebioDM <eusebioDM98@gmail.com>
2018-09-09 16:17:53 -0300

    Implemented team number check in Encounter

--- EirinDuran/EirinDuran.Domain/Fixture/Encounter.cs
+++ EirinDuran/EirinDuran.Domain/Fixture/Encounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EirinDuran.Domain.Fixture
@@ -13,14 +14,23 @@ namespace EirinDuran.Domain.Fixture
         public DateTime DateTime { get => dateTime; set => dateTime = value; }
         public Sport Sport { get => sport; set => sport = value; }
 
-        public Encounter(Sport sport, ICollection<Team> teams, DateTime dateTime)
+        public Encounter(Sport sport, IEnumerable<Team> teams, DateTime dateTime)
         {
+            ValidateNumberOfTeams(teams);
             this.sport = sport;
             this.teams = GetTeamsArray(teams);
             this.dateTime = dateTime;
         }
 
-        private Team[] GetTeamsArray(ICollection<Team> teams)
+        private void ValidateNumberOfTeams(IEnumerable<Team> teams)
+        {
+            if(teams.Count() != 2)
+            {
+                throw new InvalidNumberOfTeamsException();
+            }
+        }
+
+        private Team[] GetTeamsArray(IEnumerable<Team> teams)
         {
             Team[] array = new Team[2];
             int i = 0;
