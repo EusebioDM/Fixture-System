1c45d29c7a8ce8f4de473931f4cf463962d49daf
EusebioDM <eusebioDM98@gmail.com>
2018-09-09 16:12:37 -0300

    Implemented Encounter class

--- EirinDuran/EirinDuran.Domain/Fixture/Encounter.cs
+++ EirinDuran/EirinDuran.Domain/Fixture/Encounter.cs
@@ -6,6 +6,29 @@ namespace EirinDuran.Domain.Fixture
 {
     public class Encounter
     {
+        private Sport sport;
+        private Team[] teams;
+        private DateTime dateTime;
 
+        public DateTime DateTime { get => dateTime; set => dateTime = value; }
+        public Sport Sport { get => sport; set => sport = value; }
+
+        public Encounter(Sport sport, ICollection<Team> teams, DateTime dateTime)
+        {
+            this.sport = sport;
+            this.teams = GetTeamsArray(teams);
+            this.dateTime = dateTime;
+        }
+
+        private Team[] GetTeamsArray(ICollection<Team> teams)
+        {
+            Team[] array = new Team[2];
+            int i = 0;
+            foreach (Team team in teams)
+            {
+                array[i++] = team;
+            }
+            return array;
+        }
     }
 }
