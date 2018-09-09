baeb687112ef874e3901307ef21e82e9c2520049
EusebioDM <eusebioDM98@gmail.com>
2018-09-09 16:01:52 -0300

    Created CreateEncounterTest in Encounter Test

---
+++ EirinDuran/EirinDuran.Domain/Fixture/Encounter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EirinDuran.Domain.Fixture
+{
+    public class Encounter
+    {
+
+    }
+}
---
+++ EirinDuran/EirinDuran.Test/EncounterTest.cs
@@ -0,0 +1,63 @@
+﻿using EirinDuran.Domain.Fixture;
+using EirinDuran.Test.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace EirinDuran.Test
+{
+    [TestClass]
+    public class EncounterTest
+    {
+        private DateTime fechaMenor;
+        private DateTime fechaMayor;
+        private Team boca;
+        private Team river;
+        private List<Team> teams;
+        private Sport futbol;
+
+        [TestMethod]
+        public void CreateEncounterTest()
+        {
+            Encounter encounter = new Encounter(futbol, teams, fechaMenor);
+
+            Assert.IsTrue(encounter.DateTime.Equals(fechaMenor) && encounter.Sport.Equals(futbol));
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            fechaMenor = new DateTime(3000, 1, 20, 1, 1, 1);
+            fechaMayor = new DateTime(3000, 1, 21, 1, 1, 1);
+            boca = CreateBocaTeam();
+            river = CreateTeamThatBelongsInTheB();
+            teams = new List<Team>()
+            {
+                boca, river
+            };
+            futbol = new Sport("Futbol");
+        }
+
+        private Team CreateBocaTeam()
+        {
+            string name = "Boca Juniors";
+            Image image = GetImage(Resources.Boca);
+            return new Team(name, image);
+        }
+
+        private Team CreateTeamThatBelongsInTheB()
+        {
+            string name = "River Plate";
+            Image image = GetImage(Resources.River);
+            return new Team(name, image);
+        }
+
+        private Image GetImage(byte[] resource)
+        {
+            return new Bitmap(new MemoryStream(resource));
+        }
+    }
+}
