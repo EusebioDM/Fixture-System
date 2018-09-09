56bb8d201d1f089b1833ae6ffa78ed5192021906
Nicolás Eirin <nicoeirin@gmail.com>
2018-09-09 12:22:06 -0300

    Renamed files and more unit tests added

--- EirinDuran/EirinDuran.Test/UserTest.cs
+++ EirinDuran/EirinDuran.Test/UserTest.cs
@@ -3,14 +3,108 @@ using EirinDuran.Domain.User;
 
 namespace EirinDuran.Test
 {
+    
     [TestClass]
     public class UserTest
     {
-        [TestMethod]
-        public void CreateEmptyUserOk()
+        private User user;
+
+        [TestInitialize]
+        public void ConfigTests()
         {
-            User user  = new User();
+            user = new User(Role.Administrator, "A.Gómez", "Álvaro", "Gómez", "pass1234", "gomez@gomez.uy");
+        }
+
+        [TestMethod]
+        public void NewNotNullUserTest()
+        {
             Assert.IsNotNull(user);
+        }
+
+        [TestMethod]
+        public void UserNameOkUserTest()
+        {
+            Assert.AreEqual("A.Gómez", user.UserName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyFieldException))]
+        public void UserNameEmptyUserTest()
+        {
+            user = new User(Role.Follower, "", "Álvaro", "Gómez", "pass1234", "agomez@mail.com");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCharactersFieldExcepion))]
+        public void NameEmptyUserTest()
+        {
+            user.Name = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCharactersFieldExcepion))]
+        public void NameInvalidCharactersUserTest()
+        {
+            user.Name = "Alva334ro";
+        }
+
+        [TestMethod]
+        public void NameOkUserTest()
+        {
+            Assert.AreEqual("ÁLVARO", user.Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCharactersFieldExcepion))]
+        public void SurnameEmptyUserTest()
+        {
+            user.Surname = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCharactersFieldExcepion))]
+        public void SurnameInvalidCharactersUserTest()
+        {
+            user.Surname = "+Gomez";
+        }
+
+        [TestMethod]
+        public void SurnameOkUserTest()
+        {
+            Assert.AreEqual("GÓMEZ", user.Surname);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyFieldException))]
+        public void PasswordEmptyUserTest()
+        {
+            user.Password = "";
+        }
+
+        [TestMethod]
+        public void PasswordOkUserTest()
+        {
+            Assert.AreEqual("pass1234", user.Password);
+        }
+
+        [TestMethod]
+        public void MailOkUserTest()
+        {
+            Assert.AreEqual("gomez@gomez.uy", user.Mail);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidMailFormatException))]
+        public void InvalidMailFormatTest()
+        {
+            user.Mail = "gomez@gomez";
+        }
+
+        [TestMethod]
+        public void RoleVerificationTest()
+        {
+            bool result = user.HasRole(Role.Administrator);
+            Assert.AreEqual(true, result);
         }
     }
 }
