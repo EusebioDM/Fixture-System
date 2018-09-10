653fc971828f212eec5faa73ef4522dc45a25e2c
EusebioDM <eusebioDM98@gmail.com>
2018-09-10 17:17:41 -0300

    Finished UserRepo

--- EirinDuran/EirinDuran.DataAccess/UserRepository.cs
+++ EirinDuran/EirinDuran.DataAccess/UserRepository.cs
@@ -1,56 +1,110 @@
+using EirinDuran.Domain;
+using EirinDuran.Domain.User;
+using EirinDuran.Entities;
+using EirinDuran.Entities.Mappers;
+using EirinDuran.IDataAccess;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using EirinDuran.Domain;
-using EirinDuran.Domain.User;
-using EirinDuran.IDataAccess;
-using EirinDuran.Entities;
-using EirinDuran.Entities.Mappers;
 
-namespace EirinDuran.DataAccess {
-    public class UserRepository : IRepository<User> 
+namespace EirinDuran.DataAccess
+{
+    public class UserRepository : IRepository<User>
     {
-        private readonly Context context;
-        public UserRepository(Context aContext)
+        public void Add(User user)
         {
-            context = aContext;
-        }
-
-        public void Add (User user) 
-        {
-            UserEntity userEntity = new UserEntity(user);
-
-            context.Users.Add(userEntity);
-            context.SaveChanges();
-        }
-
-        public void Delete (User user) 
-        {
-            
-        }
-
-        public User Get (int id) 
-        {
-            UserEntity userEntity = context.Users.Find(id);
-
-            return userEntity.ToModel();
-        }
-
-        public IEnumerable<User> GetAll () 
-        {
-            List<User> userList = new List<User>();
-
-            foreach(var user in context.Users.ToList())
+            try
             {
-                userList.Add(user.ToModel());
+                TryToAdd(user);
             }
-
-            return userList;
+            catch (DbUpdateException)
+            {
+                throw new ObjectAlreadyExistsInDataBaseException();
+            }
         }
 
-        public void Update (User upToDate) 
+        private void TryToAdd(User user)
         {
-            
+            using (Context context = new Context())
+            {
+                context.Users.Add(new UserEntity(user));
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(User user)
+        {
+            try
+            {
+                TryToDelete(user);
+            }
+            catch (InvalidOperationException)
+            {
+                throw new ObjectDoesntExistsInDataBaseException();
+            }
+        }
+
+        private void TryToDelete(User user)
+        {
+            using (Context context = new Context())
+            {
+                UserEntity toDelete = context.Users.Single(u => u.Name.Equals(user.Name));
+                context.Users.Remove(toDelete);
+                context.SaveChanges();
+            }
+        }
+
+        public User Get(int id)
+        {
+            try
+            {
+                return TryToGet(id);
+            }
+            catch (InvalidOperationException)
+            {
+                throw new ObjectDoesntExistsInDataBaseException();
+            }
+        }
+
+        private User TryToGet(int id)
+        {
+            using (Context context = new Context())
+            {
+                UserEntity toReturn = context.Users.Single(t => t.Id.Equals(id));
+                return toReturn.ToModel();
+            }
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            using (Context context = new Context())
+            {
+                Func<UserEntity, User> mapEntity = u => { return u.ToModel(); };
+                return context.Users.Select(mapEntity);
+            }
+        }
+
+        public void Update(User user)
+        {
+            try
+            {
+                TryToUpdate(user);
+            }
+            catch (InvalidOperationException)
+            {
+                throw new ObjectDoesntExistsInDataBaseException();
+            }
+        }
+
+        private void TryToUpdate(User user)
+        {
+            using (Context context = new Context())
+            {
+                UserEntity toUpdate = context.Users.Single(u => u.Name.Equals(user.Name));
+                toUpdate.UpdateWith(user);
+                context.SaveChanges();
+            }
         }
     }
 }
