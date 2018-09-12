7747a05c5bd7d637dfaf7676373d3318a04a3b38
Eusebio <eusebiodm98@gmail.com>
2018-09-12 10:39:02 -0300

    Created DataAccesTest proyect

---
+++ EirinDuran/EirinDuran.DataAccessTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EirinDuran.DataAccessTest
+{
+    [TestClass]
+    public class EntityRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
