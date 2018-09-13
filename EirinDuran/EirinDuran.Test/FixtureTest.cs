6c168290cf1eb9dad0ed163113b7059d0b50d2a9
Nicolás Eirin <nicoeirin@gmail.com>
2018-09-13 16:22:22 -0300

    OddNumberOfTeamsInFixture test implemented

--- EirinDuran/EirinDuran.Test/FixtureTest.cs
+++ EirinDuran/EirinDuran.Test/FixtureTest.cs
@@ -13,13 +13,12 @@ namespace EirinDuran.Test
     [TestClass]
     public class FixtureTest
     {
-        
+
         [TestMethod]
-        [ExpectedException(typeof(InvalidNumberOfTeamsException))]
         public void SampleAutoFixtureLeagueTest()
-        {   
+        {
             Image image = GetImage(Resources.River);
-            
+
             Team boca = new Team("boca", image);
             Team cerro = new Team("cerro", image);
 
@@ -33,7 +32,7 @@ namespace EirinDuran.Test
             teams.Add(boca);
             teams.Add(cerro);
 
-            DateTime date = new DateTime(2018, 10, 07, 18, 30, 00); 
+            DateTime date = new DateTime(2018, 10, 07, 18, 30, 00);
 
             Encounter expected = new Encounter(football, teams, date);
 
@@ -43,6 +42,32 @@ namespace EirinDuran.Test
 
             Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNumberOfTeamsException))]
+        public void OddNumberOfTeamsInFixture()
+        {
+            Image image = GetImage(Resources.River);
+
+            Team boca = new Team("boca", image);
+            Team cerro = new Team("cerro", image);
+            Team river = new Team("River Plate", image);
+
+            Sport football = new Sport("football");
+
+            football.AddTeam(boca);
+            football.AddTeam(cerro);
+            football.AddTeam(river);
+
+            List<Team> teams = new List<Team>();
+
+            DateTime date = new DateTime(2018, 10, 07, 18, 30, 00);
+
+            IFixtureGenerator leagueFixture = new LeagueFixture();
+
+            List<Encounter> result = leagueFixture.GenerateFixture(teams, date).ToList();
+        }
+
 
         private Image GetImage(byte[] resource)
         {
