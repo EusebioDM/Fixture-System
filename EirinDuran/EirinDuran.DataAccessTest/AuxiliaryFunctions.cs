acfbbb1961b173415ed3d813e443aaa9caf22a90
EusebioDM <eusebioDM98@gmail.com>
2018-09-14 16:18:46 -0300

    Created AddUserTest in UserRepoTest

---
+++ EirinDuran/EirinDuran.DataAccessTest/AuxiliaryFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EirinDuran.DataAccessTest
+{
+    internal static class HelperFunctions<T>
+    {
+        public static bool CollectionsHaveSameElements(IEnumerable<T> first, IEnumerable<T> second)
+        {
+            HashSet<T> firstSet = new HashSet<T>(first);
+            HashSet<T> secondSet = new HashSet<T>(second);
+
+            return firstSet.SetEquals(secondSet);
+        }
+    }
+}
