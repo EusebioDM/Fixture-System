8e401471b1fcce026134810a13b5e393ac3aff89
EusebioDM <eusebioDM98@gmail.com>
2018-09-16 20:11:21 -0300

    Configured Startup Dependency Injection

--- EirinDuran/EirinDuran.WebApi/Startup.cs
+++ EirinDuran/EirinDuran.WebApi/Startup.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using EirinDuran.DataAccess;
+using EirinDuran.Domain.User;
+using EirinDuran.IDataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using EirinDuran.IDataAccess;
-using EirinDuran.DataAccess;
-using EirinDuran.Domain.User;
-using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EirinDuran.WebApi
 {
@@ -28,11 +28,20 @@ namespace EirinDuran.WebApi
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {   
+        {
             services.AddScoped<IRepository<User>, UserRepository>();
-            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("EirinDuranDB"), b => b.MigrationsAssembly("EirinDuran.WebApi")));
-            services.AddScoped<IContext>(provider => provider.GetService<Context>());
+            services.AddScoped<IContextFactory>(provider => provider.GetService<ContextFactory>());
+            services.AddScoped<IContextFactory, ContextFactory>();
+            services.AddScoped<DbContextOptions<Context>>(CreateDbContextOptions);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        private DbContextOptions<Context> CreateDbContextOptions(IServiceProvider serviceProvider)
+        {
+            DbContextOptionsBuilder<Context> builder = new DbContextOptionsBuilder<Context>().UseSqlServer(Configuration.GetConnectionString("EirinDuranDB"),
+                                                                                       b => b.MigrationsAssembly("EirinDuran.WebApi")).UseLazyLoadingProxies();
+            return builder.Options;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
