e4d51633e08fae28899f09428b923c9fbffe3637
Nicolás Eirin <nicoeirin@gmail.com>
2018-09-19 09:03:16 -0300

    CreateAutoGeneratedFixture test added

--- EirinDuran/EirinDuran.WebApi/Migrations/20180919004940_migration_20180918.Designer.cs
+++ EirinDuran/EirinDuran.WebApi/Migrations/20180919004940_migration_20180918.Designer.cs
@@ -4,14 +4,16 @@ using EirinDuran.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EirinDuran.WebApi.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20180919004940_migration_20180918")]
+    partial class migration_20180918
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
