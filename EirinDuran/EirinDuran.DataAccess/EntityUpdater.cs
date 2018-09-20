f98079b6141ff73dd48423e5947f8c734839ecc3
EusebioDM <eusebioDM98@gmail.com>
2018-09-19 23:27:07 -0300

    Refractored EntityUpdater

--- EirinDuran/EirinDuran.DataAccess/EntityUpdater.cs
+++ EirinDuran/EirinDuran.DataAccess/EntityUpdater.cs
@@ -17,35 +17,59 @@ namespace EirinDuran.DataAccess
 
             while (entitiesLeftToUpdate.Count() > 0)
             {
-                object next = entitiesLeftToUpdate.Peek();
-                using (Context context = contextFactory.CreateDbContext(new string[0]))
-                {
-                    context.ChangeTracker.TrackGraph(next, n => UpdateNode(context, entitiesLeftToUpdate, n));
-                    context.SaveChanges();
-                }
-                entitiesLeftToUpdate.Dequeue();
+                UpdateRootEntityAndItsChildrenIfPossible(contextFactory, entitiesLeftToUpdate);
             }
         }
 
-        private void UpdateNode(Context context, Queue<object> leftToUpdate, EntityEntryGraphNode n)
+        private void UpdateRootEntityAndItsChildrenIfPossible(IDesignTimeDbContextFactory<Context> contextFactory, Queue<object> entitiesLeftToUpdate)
         {
-            var entry = n.Entry;
-            var sEntry = n.SourceEntry;
-            if (!EntryExistsInChangeTracker(context, entry))
+            object rootEntityToUpdate = entitiesLeftToUpdate.Peek();
+            using (Context context = contextFactory.CreateDbContext(new string[0]))
             {
-                if (EntryExistsInDb(context, entry))
-                {
-                    entry.State = EntityState.Modified;
-                }
-                else
-                {
-                    entry.State = EntityState.Added;
-                }
+                TraverseEntityGraphUpdatingWhenPossible(entitiesLeftToUpdate, rootEntityToUpdate, context);
+                context.SaveChanges();
+            }
+            entitiesLeftToUpdate.Dequeue();
+        }
+
+        private void TraverseEntityGraphUpdatingWhenPossible(Queue<object> entitiesLeftToUpdate, object rootEntityToUpdate, Context context)
+        {
+            context.ChangeTracker.TrackGraph(rootEntityToUpdate, n => UpdateNodeRecursively(context, entitiesLeftToUpdate, n));
+        }
+
+        private void UpdateNodeRecursively(Context context, Queue<object> toUpdateQueue, EntityEntryGraphNode node)
+        {
+            EntityEntry entry = node.Entry;
+            EntityEntry fatherNode = node.SourceEntry;
+
+            if (EntryExistsInChangeTracker(context, entry)) // Entity is already being tracked in a different node so the current context cant track it
+            {
+                EnqueueFatherNode(toUpdateQueue, fatherNode);
             }
             else
             {
-                if (!leftToUpdate.Contains(sEntry.Entity))
-                    leftToUpdate.Enqueue(sEntry.Entity);
+                SetEntityAsModifiedOrAdded(context, entry);
+            }
+        }
+
+        private void SetEntityAsModifiedOrAdded(Context context, EntityEntry entry)
+        {
+            if (EntryExistsInDb(context, entry))
+            {
+                entry.State = EntityState.Modified;
+            }
+            else
+            {
+                entry.State = EntityState.Added;
+            }
+        }
+
+        private void EnqueueFatherNode(Queue<object> leftToUpdate, EntityEntry fatherNode)
+        {
+            bool canEnqueueWithoutGettingStuckInLoop = !leftToUpdate.Contains(fatherNode.Entity);
+            if (canEnqueueWithoutGettingStuckInLoop)
+            {
+                leftToUpdate.Enqueue(fatherNode.Entity); // Entity is added to the queue so it can be added in a different context
             }
         }
 
