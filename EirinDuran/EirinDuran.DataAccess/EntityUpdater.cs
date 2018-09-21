04eb634c8d4fbc4aa6103cfb2e6f8387a809059e
EusebioDM <eusebioDM98@gmail.com>
2018-09-21 17:46:12 -0300

    Fixed edge case in Entity Repo

--- EirinDuran/EirinDuran.DataAccess/EntityUpdater.cs
+++ EirinDuran/EirinDuran.DataAccess/EntityUpdater.cs
@@ -13,36 +13,83 @@ namespace EirinDuran.DataAccess
         public void UpdateGraph(IDesignTimeDbContextFactory<Context> contextFactory, Entity entity)
         {
             Queue<object> entitiesLeftToUpdate = new Queue<object>();
+            HashSet<object> set = new HashSet<object>();
             entitiesLeftToUpdate.Enqueue(entity);
 
             while (entitiesLeftToUpdate.Count() > 0)
             {
-                UpdateRootEntityAndItsChildrenIfPossible(contextFactory, entitiesLeftToUpdate);
+                UpdateRootEntityAndItsChildrenIfPossible(contextFactory, entitiesLeftToUpdate, set);
+            }
+
+            using (Context context = contextFactory.CreateDbContext(new string[0]))
+            {
+                EntityEntry entry = context.Entry(entity);
+                object key = HelperFunctions<Entity>.GetKey(entry);
+                Entity root = context.Find<Entity>(key);
+                //context.ChangeTracker.TrackGraph(root,n,f => Callback(n,set,f));
+                //context.ChangeTracker.TrackGraph<Entity>(root, root,(n, f) => Callback(n, f, set));
+                Rec(context, context.Entry(root), set);
+                context.SaveChanges();
             }
         }
 
-        private void UpdateRootEntityAndItsChildrenIfPossible(IDesignTimeDbContextFactory<Context> contextFactory, Queue<object> entitiesLeftToUpdate)
+        private void Rec(Context context, EntityEntry entry, HashSet<object> set)
+        {
+            object key = HelperFunctions<Entity>.GetKey(entry);
+            if (!set.Contains(key))
+                entry.State = EntityState.Deleted;
+            foreach (var p in entry.Navigations)
+            {
+                p.Load();
+                try
+                {
+                    IEnumerable<object> l = (p.CurrentValue as IEnumerable<object>).Cast<object>().ToList();
+                    foreach (object o in l)
+                    {
+                        var e = context.Entry(o);
+                        Rec(context, e, set);
+                    }
+                }
+                catch (Exception)
+                {
+                    var e = context.Entry(p.CurrentValue);
+                    Rec(context, e, set);
+                }
+            }
+        }
+
+        private bool Callback(EntityEntryGraphNode node, Entity entity, HashSet<object> set)
+        {
+            object key = HelperFunctions<Entity>.GetKey(node.Entry);
+            if (!set.Contains(key))
+                node.Entry.State = EntityState.Deleted;
+            return true;
+        }
+
+        private void UpdateRootEntityAndItsChildrenIfPossible(IDesignTimeDbContextFactory<Context> contextFactory, Queue<object> entitiesLeftToUpdate, HashSet<object> set)
         {
             object rootEntityToUpdate = entitiesLeftToUpdate.Peek();
+
             using (Context context = contextFactory.CreateDbContext(new string[0]))
             {
-                TraverseEntityGraphUpdatingWhenPossible(entitiesLeftToUpdate, rootEntityToUpdate, context);
+                TraverseEntityGraphUpdatingWhenPossible(entitiesLeftToUpdate, rootEntityToUpdate, context, set);
                 context.SaveChanges();
             }
             entitiesLeftToUpdate.Dequeue();
         }
 
-        private void TraverseEntityGraphUpdatingWhenPossible(Queue<object> entitiesLeftToUpdate, object rootEntityToUpdate, Context context)
+        private void TraverseEntityGraphUpdatingWhenPossible(Queue<object> entitiesLeftToUpdate, object rootEntityToUpdate, Context context, HashSet<object> set)
         {
-            Action<EntityEntryGraphNode> updateNodeRecursivelyAction = n => UpdateNodeRecursively(context, entitiesLeftToUpdate, n);
+            Action<EntityEntryGraphNode> updateNodeRecursivelyAction = n => UpdateNodeRecursively(context, entitiesLeftToUpdate, n, set);
 
             context.ChangeTracker.TrackGraph(rootEntityToUpdate, updateNodeRecursivelyAction);
         }
 
-        private void UpdateNodeRecursively(Context context, Queue<object> toUpdateQueue, EntityEntryGraphNode node)
+        private void UpdateNodeRecursively(Context context, Queue<object> toUpdateQueue, EntityEntryGraphNode node, HashSet<object> set)
         {
             EntityEntry current = node.Entry;
             EntityEntry fatherNode = node.SourceEntry;
+            set.Add(HelperFunctions<Entity>.GetKey(current));
 
             if (EntryExistsInChangeTracker(context, current)) // Entity is already being tracked in a different node so the current context cant track it
             {
@@ -50,14 +97,25 @@ namespace EirinDuran.DataAccess
             }
             else
             {
-                SetEntityAsModifiedOrAdded(context, current);
+                SetEntityAsModifiedOrAdded(context, current, node);
             }
         }
 
-        private void SetEntityAsModifiedOrAdded(Context context, EntityEntry entry)
+        private void SetEntityAsModifiedOrAdded(Context context, EntityEntry entry, EntityEntryGraphNode node)
         {
             if (EntryExistsInDb(context, entry))
             {
+                //var db = context.Model.
+                //foreach (var p in db.Collections)
+                //{
+                //    IEnumerable<object> dbCollection = (IEnumerable<object>)p.CurrentValue;
+                //    IEnumerable<object> oCollection = (IEnumerable<object>)entry.Collection(p.Metadata.Name).CurrentValue;
+                //    foreach (var obj in dbCollection.Where(o => !oCollection.Any(ob => HelperFunctions<object>.EntriesAreEqual(context, o, ob))))
+                //    {
+                //        context.Entry(obj).State = EntityState.Deleted;
+                //    }
+                //}
+
                 entry.State = EntityState.Modified;
             }
             else
