d796955a5a7e82329f04b5b7ad24dd539aa78ef6
Nicolás Eirin <nicoeirin@gmail.com>
2018-10-21 16:33:25 -0300

    Material table added

--- FrontEnd/src/app/components/users-list/users-list.component.ts
+++ FrontEnd/src/app/components/users-list/users-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../classes/user';
 import { UsersService } from '../../services/users.service';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-users-list',
@@ -10,13 +11,19 @@ import { UsersService } from '../../services/users.service';
 export class UsersListComponent implements OnInit {
   constructor(private usersService: UsersService) { }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   users: Array<User>;
+
+  displayedColumns: string[] = ['userName', 'name', 'surname', 'mail'];
+  dataSource;
 
   ngOnInit() {
     this.usersService.getUsers().subscribe(
       ((data: Array<User>) => this.result(data)),
       ((error: any) => console.log(error))
     );
+    this.dataSource = new MatTableDataSource<User>(this.users);
+    this.dataSource.paginator = this.paginator;
   }
 
   private result(data: Array<User>): void {
@@ -24,3 +31,4 @@ export class UsersListComponent implements OnInit {
     console.log(this.users);
   }
 }
+
