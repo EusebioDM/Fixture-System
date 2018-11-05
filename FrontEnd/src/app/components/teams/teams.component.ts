24002fff12a47872d9322d46601b04001ae19a19
Nicolás Eirin <nicoeirin@gmail.com>
2018-11-05 20:07:41 -0300

    Refactored temas component

--- FrontEnd/src/app/components/teams/teams.component.ts
+++ FrontEnd/src/app/components/teams/teams.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Team } from 'src/app/classes/team';
-import { MatPaginator, MatTableDataSource, MatDialog  } from '@angular/material';
+import { MatPaginator, MatTableDataSource, MatDialog, MatSort } from '@angular/material';
 import { TeamsService } from 'src/app/services/teams.service';
 import { AddTeamComponent } from '../add-team/add-team.component';
 
@@ -13,28 +13,39 @@ export class TeamsComponent implements OnInit {
 
   constructor(private teamsService: TeamsService, private dialog: MatDialog) { }
 
+  @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   teams: Array<Team>;
 
   displayedColumns: string[] = ['name', 'sportName', 'logo', 'btnModify', 'btnDelete'];
   dataSource;
+  searchKey: string;
 
   ngOnInit() {
     this.teamsService.getTeams().subscribe(
       ((data: Array<Team>) => this.result(data)),
       ((error: any) => console.log(error))
     );
-
-    this.dataSource = new MatTableDataSource<Team>(this.teams);
-    this.dataSource.paginator = this.paginator;
   }
 
   private result(data: Array<Team>): void {
     this.teams = data;
-    console.log(this.teams);
+    console.log('Este es el array de teams: ' + this.teams);
+    this.loadTableDataSource();
   }
 
-  openDialogAddTeam() {
+  private loadTableDataSource() {
+    this.dataSource = new MatTableDataSource<Team>(this.teams);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.filterPredicate = (data, filter) => {
+      return this.displayedColumns.some(ele => {
+        return ele !== 'btnModify' && ele !== 'btnDelete' && data[ele].toLowerCase().indexOf(filter) !== -1;
+      });
+    };
+  }
+
+  onInsert() {
     const dialogRef = this.dialog.open(AddTeamComponent);
 
     dialogRef.afterClosed().subscribe(result => {
@@ -44,5 +55,14 @@ export class TeamsComponent implements OnInit {
         this.ngOnInit();
       }
     });
+  }
+
+  onSearchClear() {
+    this.searchKey = '';
+    this.applyFilter();
+  }
+
+  applyFilter() {
+    this.dataSource.filter = this.searchKey.trim().toLowerCase();
   }
 }
